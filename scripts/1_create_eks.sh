0a9c1d51b44f3332769333793b4785e6fb64829f
paravatha <prasad.paravatha@gmail.com>
2023-07-05 22:39:47 -0400

    update eks setup

--- scripts/1_create_eks.sh
+++ scripts/1_create_eks.sh
@@ -3,7 +3,7 @@ export CLUSTER_NAME=kubeflow
 export CLUSTER_REGION=us-east-1
 export KUBEFLOW_RELEASE_VERSION=v1.7.0
 export AWS_RELEASE_VERSION=v1.7.0-aws-b1.0.2
-export AWS_ACCOUNT=
+export AWS_ACCOUNT=981864830773
 
 git clone https://github.com/awslabs/kubeflow-manifests.git && cd kubeflow-manifests
 git checkout ${AWS_RELEASE_VERSION}
