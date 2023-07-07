76808377e5e10ca9ad2ef02d6bd2de12eb245626
paravatha <prasad.paravatha@gmail.com>
2023-07-07 19:57:24 -0400

    update sh

---
+++ scripts/0_init_cli.sh
@@ -0,0 +1,6 @@
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
--- scripts/kubectl.sh
+++
@@ -1,2 +0,0 @@
-echo 'alias k=kubectl' >>~/.bashrc
-echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
