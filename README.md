d0119af5dc0a53a5bfa15bce9721c6aaf29c8e64
paravatha <prasad.paravatha@gmail.com>
2023-07-07 20:01:53 -0400

    update sh

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # kubeflow-on-eks
 
-https://github.com/paravatha/kubeflow-on-eks
+git clone https://github.com/paravatha/kubeflow-on-eks
 cd kubeflow-on-eks
--- scripts/0_init_cli.sh
+++ scripts/0_init_cli.sh
@@ -1,6 +1,10 @@
+kubectl version
+
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+
+source ~/.bashrc
