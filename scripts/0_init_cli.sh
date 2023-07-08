238c0a5d50c06234fbaea7449a2beab116e63e71
paravatha <prasad.paravatha@gmail.com>
2023-07-07 21:48:09 -0400

    update sh

--- scripts/0_init_cli.sh
+++ scripts/0_init_cli.sh
@@ -1,15 +1,10 @@
 kubectl version
 
-echo -e "\nalias gs='git status '" >> ~/.bashrc
-echo -e "\nalias gpl='git pull '" >> ~/.bashrc
-echo -e "\nalias gps='git push '" >> ~/.bashrc
-
-echo 'alias c=clear' >>~/.bashrc
-echo 'alias k=kubectl' >>~/.bashrc
-echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
-
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
+cp ../config/.bashrc ~/.bashrc
+chmod 755 ~/.bashrc
+cat ~/.bashrc
 source ~/.bashrc
--- scripts/4_delete_eks.sh
+++ scripts/4_delete_eks.sh
@@ -1,6 +1,8 @@
 #export AWS_PROFILE=kubeflow
 export CLUSTER_NAME=kubeflow
 export CLUSTER_REGION=us-east-1
+eksctl scale nodegroup --cluster=${CLUSTER_NAME} --nodes=0 managed-ng-t2
+eksctl scale nodegroup --cluster=${CLUSTER_NAME} --nodes=0 managed-ng-t3
 
 eksctl delete cluster -f config/cluster.yaml
 
