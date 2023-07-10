a84344256818f153b38fbd9c6c12f765c5e83374
paravatha <prasad.paravatha@gmail.com>
2023-07-09 22:33:57 -0400

    update sh

--- scripts/scale_ng.sh
+++ scripts/scale_ng.sh
@@ -2,5 +2,5 @@ export AWS_PROFILE=kubeflow
 export CLUSTER_NAME=kubeflow
 export CLUSTER_REGION=us-east-1
 
-eksctl scale nodegroup --name ${CLUSTER_NAME}  --nodes=3 --name=managed-ng-t2
-eksctl scale nodegroup --name ${CLUSTER_NAME}  --nodes=3 --name=managed-ng-t3
+eksctl scale nodegroup --cluster=${CLUSTER_NAME} --name=managed-ng-t2 --nodes=3
+eksctl scale nodegroup --cluster=${CLUSTER_NAME} --name=managed-ng-t3 --nodes=3
