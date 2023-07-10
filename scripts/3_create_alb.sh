23b6dac67fd924b685025aed0d5a5fc9e9e964f1
paravatha <prasad.paravatha@gmail.com>
2023-07-09 22:36:24 -0400

    update sh

--- scripts/3_create_alb.sh
+++ scripts/3_create_alb.sh
@@ -6,6 +6,6 @@ export AWS_RELEASE_VERSION=v1.7.0-aws-b1.0.2
 
 # update tests/e2e/utils/load_balancer/config.yaml
 cp config/lb_config.yaml kubeflow-manifests/tests/e2e/utils/load_balancer/config.yaml
-cd tests/e2e
+cd kubeflow-manifests/tests/e2e
 python3.8 -m pip install -r requirements.txt
 PYTHONPATH=.. python3.8 utils/load_balancer/setup_load_balancer.py
